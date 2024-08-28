d940eac4e024674c7a6b89210033129b20d8bd3d
Austin Elke <103085481+TheGiantTreetrunk@users.noreply.github.com>
2024-08-27 20:03:48 -0700

    added weapon tables

--- assets/scripts/rpg_weapons.js
+++ assets/scripts/rpg_weapons.js
@@ -1,4 +1,169 @@
 //weapon system
 
+item_0 = {
+    name: "scythe",
+    src: "assets/sprites/sprite_item_5.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
 
+item_1 = {
+    name: "hoe",
+    src: "assets/sprites/sprite_item_6.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
 
+item_2 = {
+    name: "pickel fork",
+    src: "assets/sprites/sprite_item_7.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
+
+item_3 = {
+    name: "bow",
+    src: "assets/sprites/sprite_item_8.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
+
+item_4 = {
+    name: "trident",
+    src: "assets/sprites/sprite_item_10.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
+
+item_5 = {
+    name: "picaxe",
+    src: "assets/sprites/sprite_item_11.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
+
+item_6 = {
+    name: "axe",
+    src: "assets/sprites/sprite_item_12.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
+
+item_7 = {
+    name: "mallete",
+    src: "assets/sprites/sprite_item_13.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
+
+item_8 = {
+    name: "spiked chain ball",
+    src: "assets/sprites/sprite_item_14.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
+
+item_9 = {
+    name: "sledgehammer",
+    src: "assets/sprites/sprite_item_15.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
+
+item_10 = {
+    name: "maul",
+    src: "assets/sprites/sprite_item_16.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
+
+item_11 = {
+    name: "pitchfork",
+    src: "assets/sprites/sprite_item_17.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
+
+item_12 = {
+    name: "rake",
+    src: "assets/sprites/sprite_item_18.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
+
+item_13 = {
+    name: "hockey stick",
+    src: "assets/sprites/sprite_item_19.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
+
+item_14 = {
+    name: "chair",
+    src: "assets/sprites/sprite_item_21.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
+
+item_15 = {
+    name: "cooking pot",
+    src: "assets/sprites/sprite_item_22.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
+
+item_16 = {
+    name: "frying pan",
+    src: "assets/sprites/sprite_item_23.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
+
+item_17 = {
+    name: "crossbow",
+    src: "assets/sprites/sprite_item_24.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
+
+item_18 = {
+    name: "long bow",
+    src: "assets/sprites/sprite_item_25.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
+
+item_19 = {
+    name: "dual short swords",
+    src: "assets/sprites/sprite_item_30.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
+
+item_20 = {
+    name: "long sword",
+    src: "assets/sprites/sprite_item_31.png",
+    tier: 1,
+    dice: 4,
+    dice_total: 1 
+};
