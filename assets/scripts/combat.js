7175a73d681522d502920f65b16356f0a79c146f
Austin Elke <103085481+TheGiantTreetrunk@users.noreply.github.com>
2024-09-11 22:11:59 -0700

    adding files

---
+++ assets/scripts/combat.js
@@ -0,0 +1,9 @@
+/* Combat System */
+
+var player_combat = {
+    isattack: 0, //is attacking or not
+    cur_side: 0, //0 left, 1 top, 2 right
+    stanima: 100 //energy the player still has left before recharge.
+
+    //will reference player stats otherwise everything else will be modular
+};
